4b7ac7ece53b0d0296d362d5fbbe4338dcebb3aa
emonmahdi <developeremon1997@gmail.com>
2025-05-14 14:15:35 +0600

    video popup solve done

--- assets/js/videoPopup.js
+++ assets/js/videoPopup.js
@@ -1,12 +1,32 @@
-const videoModal = document.getElementById("videoModal");
-      const youtubeVideo = document.getElementById("youtubeVideo");
+// const videoModal = document.getElementById("videoModal");
+//       const youtubeVideo = document.getElementById("youtubeVideo");
     
-      const videoURL = "https://www.youtube.com/embed/NWHU8wKrhok?si=xmnG0vZKztiy2F5z";
+//       const videoURL = "https://www.youtube.com/embed/NWHU8wKrhok?si=xmnG0vZKztiy2F5z";
     
-      videoModal.addEventListener("show.bs.modal", function () {
-        youtubeVideo.src = videoURL;
-      });
+//       videoModal.addEventListener("show.bs.modal", function () {
+//         youtubeVideo.src = videoURL;
+//       });
     
-      videoModal.addEventListener("hidden.bs.modal", function () {
-        youtubeVideo.src = "";
-      });
+//       videoModal.addEventListener("hidden.bs.modal", function () {
+//         youtubeVideo.src = "";
+//       });
+
+
+// video-modal.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modals = document.querySelectorAll(".video-modal");
+
+  modals.forEach((modal) => {
+    const youtubeFrame = modal.querySelector("iframe");
+    const videoURL = modal.getAttribute("data-video-url");
+
+    modal.addEventListener("show.bs.modal", function () {
+      youtubeFrame.src = videoURL;
+    });
+
+    modal.addEventListener("hidden.bs.modal", function () {
+      youtubeFrame.src = "";
+    });
+  });
+});
